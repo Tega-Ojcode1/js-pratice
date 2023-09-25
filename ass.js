ba2a605f8e93c70edfdc38a1217230d230cf7541
Tega-Ojcode1 <ojevwetega3@gmail.com>
2023-09-25 17:50:14 +0100

    added new js file

---
+++ ass.js
@@ -0,0 +1,6 @@
+
+
+let n0 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for (let i = 0; i <= n0.length; i++){
+console.log(i)
+}
---
+++ index-loops.js
@@ -0,0 +1,89 @@
+
+
+// for (let i = 1; i <=6; i++){
+//     console.log(i)
+// }
+
+
+let fruit = ['watermellon', 'bannana', 'apple']
+
+for (i in fruit) {
+    console.log(fruit[i]);
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log(fruit.length)
+
+// // for (let j = 0; j < fruit.length; j++) {
+// //     console.log(fruit[j]);
+// // } 
+
+//     for (let t = 2 ; t <10; t+=2){
+// console.log(t);
+//     }
+// fall
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
