5cb196bc3dc34313fc2d7d108f5a46081cf687c3
Tega-Ojcode1 <ojevwetega3@gmail.com>
2023-09-25 18:12:56 +0100

    updated assgnment

--- ass.js
+++ ass.js
@@ -1,6 +1,13 @@
 
 
-let n0 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-for (let i = 0; i <= n0.length; i++){
-console.log(i)
+const numbers = [1, 2, 3, 4, 5,6,7];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+
+    sum += numbers[i];
 }
+
+
+console.log(sum);
