bf38d10a2602d3e723e87dda4fa57653151ee9fc
Tega-Ojcode1 <ojevwetega3@gmail.com>
2023-09-27 16:47:49 +0100

    updated assgnment

---
+++ loops.js
@@ -0,0 +1,94 @@
+/*
+Question:
+Question: Write a for loop to print numbers from 1 to 10.*/
+for (let i = 1; i <=10; i++){
+    console.log(i)
+}
+
+/*
+Question: Given an array of fruits(e.g., ["apple", "banana", "cherry",
+    "date"]), write a for loop to print each fruit on a new line.  */
+
+
+
+
+     /*   Question: Create an array of numbers and use a for loop to 
+calculate their sum.*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+
+    sum += numbers[i];
+}
+
+
+console.log(sum);
+
+let fruit = ['watermellon', 'bannana', 'apple']
+
+for (i in fruit) {
+    console.log(fruit[i]);
+}
+
+   /* Question: Given an array of names, write a for loop to check if a
+specific name(e.g., "Alice") exists in the array. */
+
+const names = ["tega ", 'micah', 'imoke']
+for (let j = names; j == names[2]; j = names) {
+    console.log(j[2]);
+}
+
+   /* Question: Generate an array of even numbers from 2 to 20 using a
+for loop.  */
+let i = 2
+let no = []
+no = i
+for (let i = 2; i < 21; i += 2) {
+    console.log(i);
+}
+
+  /*  Question: Given an array of characters, write a for loop to reverse
+the order of the characters in the array. */
+
+
+
+
+   /* Question: Create two arrays of names(e.g., firstNames and
+lastNames) and use a for loop to concatenate them into a single 
+array of full names.*/
+
+
+
+
+
+   /* Question: Write a for loop to determine and print all prime numbers
+between 1 and 50.*/
+
+
+
+
+
+
+
+/*Question: Given an array of temperatures, use a for loop to find 
+and print temperatures above 30°C and below 0°C.*/
+
+
+
+
+
+
+
+
+
+   /* Question: Given an array of numbers, use a for...in loop to 
+double the value of each element in the array.
+    er and print only the ages above 18.*/
+
+
+   
+
+
